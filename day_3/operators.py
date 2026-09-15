1f26c877b6e752ae32c4bd3a189e3225c257aff1
askaicode <askaicode@gmail.com>
2026-09-15 13:11:15 +0300

    Day 3: operators - exo 1 to 6

---
+++ day_3/operators.py
@@ -0,0 +1,23 @@
+# 1 to 4
+'''age = 24
+height = 177.50
+complex_number = 4 + 4j
+base = int(input('Enter the base of the triangle : '))
+height = int(input('Enter the height of the triangle : '))
+area_of_triangle = int(0.5 * base * height)
+print('The area of the triangle is ', area_of_triangle)
+# Triangle perimeter
+a = int(input('Enter side a : '))
+b = int(input('Enter side b : '))
+c = int(input('Enter side c : '))
+perimeter_of_triangle = a + b + c
+print('The perimeter of the triangle is ', perimeter_of_triangle)
+'''
+# 6 Calculate rectangle area = length * width
+# perimeter = 2 * (length + width)
+width = int(input('Enter the width : '))
+length = int(input('Enter the length : '))
+area_of_rectangle = length * width
+perimeter_of_rectangle = 2 * (length + width)
+print('The area of rectangle is : ', area_of_rectangle)
+print('The perimeter of rectangle is : ', perimeter_of_rectangle)
