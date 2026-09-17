acd1f9adf117f5bda35b16274a7ec9aa8e416888
askaicode <askaicode@gmail.com>
2026-09-17 10:56:26 +0300

    day 3 : exo8-slope m, x-intercept, y-intercept

--- day_3/operators.py
+++ day_3/operators.py
@@ -15,9 +15,29 @@ print('The perimeter of the triangle is ', perimeter_of_triangle)
 '''
 # 6 Calculate rectangle area = length * width
 # perimeter = 2 * (length + width)
-width = int(input('Enter the width : '))
+'''width = int(input('Enter the width : '))
 length = int(input('Enter the length : '))
 area_of_rectangle = length * width
 perimeter_of_rectangle = 2 * (length + width)
 print('The area of rectangle is : ', area_of_rectangle)
 print('The perimeter of rectangle is : ', perimeter_of_rectangle)
+
+# 7 Circle : area = pi * r * r, circumference = 2 * pi * r where pi + 3.14
+PI = 3.14
+radius = float(input('Enter the radius of circle : '))
+area_of_circle = PI * radius * radius
+print('The area of circle is : ', area_of_circle)
+circumference = 2 * PI * radius
+print('The circumference of circle is : ', circumference)
+'''
+
+# 8 Calculating slope, x-intercept and y-intercept of y = 2x - 2
+# y = mx + b
+m = 2
+b = -2
+x_intercept = -(b) / m
+y_intercept = b
+
+print('Slope(m) : ', m)
+print('x-intercept : ', x_intercept)
+print('y-intercept : ', y_intercept)
