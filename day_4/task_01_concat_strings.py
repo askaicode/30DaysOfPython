1e24876cf10c50a3b51e6e40a25ffb0fd91b6723
askaicode <askaicode@gmail.com>
2026-09-18 13:49:55 +0300

    concat words with format and join (task 01)

---
+++ day_4/task_01_concat_strings.py
@@ -0,0 +1,8 @@
+word1 = 'Thirty'
+word2 = 'Days'
+word3 = 'Of'
+word4 = 'Python'
+result = '{} {} {} {}'.format(word1, word2, word3, word4)
+result2 = ' '.join([word1, word2, word3, word4])
+print(result)
+print(result2)
