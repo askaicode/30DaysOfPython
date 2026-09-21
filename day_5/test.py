c2d9d87f500ed4db161cc21ad18903123bf46ae9
askaicode <askaicode@gmail.com>
2026-09-21 17:26:35 +0300

    Day 5 : testing

---
+++ day_5/test.py
@@ -0,0 +1,20 @@
+# Day5 : Lists
+
+lst1 = list()
+lst2 = []
+print(lst1)
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']
+web_techs = ['HTLM', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB']
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+
+# Print the lists and its length
+print('Fruits : ', fruits)
+print('Number of fruits : ', len(fruits))
+print('Vegetables : ', vegetables)
+print('Number of vegetables : ', len(vegetables))
+print('Number of animal products : ', animal_products)
+print('Web technologies : ', web_techs)
+print('Countries : ', countries)
+print('Number of countries : ', len(countries))
